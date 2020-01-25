a5b90245c1a923d6ac4c60ee2fb060c76768f229
JorisBeemster <joris_beemster@icloud.com>
2020-01-25 15:33:04 +0100

    AHN data is downloaded

--- main.R
+++ main.R
@@ -33,6 +33,20 @@ water_classification = raster_thresholding(MNDWI)
 # boundary detection
 coastlines = boundary_detection(water_classification)
 
-
-
-
+# download AHN data
+AHN_urls = c('https://geodata.nationaalgeoregister.nl/ahn3/extract/ahn3_5m_dtm/M5_49CZ2.ZIP',
+            'https://geodata.nationaalgeoregister.nl/ahn3/extract/ahn3_5m_dtm/M5_49DZ1.ZIP',
+            'https://geodata.nationaalgeoregister.nl/ahn3/extract/ahn3_5m_dtm/M5_55AN2.ZIP',
+            'https://geodata.nationaalgeoregister.nl/ahn3/extract/ahn3_5m_dtm/M5_55BN1.ZIP')
+AHN_mapsheets =  c('49CZ2', '49DZ1', '55AN2', '55BN1')
+for (i in 1:length(AHN_urls)){
+  filename = paste(data_folder, '/', AHN_mapsheets[i], sep = '')
+  download.file(url = AHN_urls[i], destfile = filename, method = 'auto', overwrite = TRUE)
+  unzip(filename, exdir = data_folder, overwrite = TRUE)
+  file.remove(filename)
+}
+AHN_files = list.files(data_folder, pattern = glob2rx('m5*'), full.names = TRUE)
+AHN_5m = merge(raster(AHN_files[1]), 
+               raster(AHN_files[2]), 
+               raster(AHN_files[3]), 
+               raster(AHN_files[4]))
